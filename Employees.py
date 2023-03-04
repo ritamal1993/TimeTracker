c7f97e638c4f0224ce7c88b6f511b0644cb69c1c
Rita <rita46297@gmail.com>
2023-03-04 14:32:28 +0200

    add employee using path

--- Employees.py
+++ Employees.py
@@ -1,5 +1,5 @@
 
-
+import pandas as pd
 class Employees:
     def __init__(self, employee_id, name, phone,age):
         self.employee_id = employee_id
@@ -42,21 +42,9 @@ def addemployee():
 #Add employee from file
 
 def addemployeebypath(path):
-    print("Adding a new employees, from path\n"+path)
-    employee_id = int(input("Enter your employee ID\n"))
-    name = input("Enter your name\n")
-    phone = input("Enter your department\n")
-    age = input("Enter your age\n")
-
-
-    with open('database.csv', "a") as f:
-        employee_info = ""
-        employee_info += "%s," % employee_id
-        employee_info += "%s," % name
-        employee_info += "%s," % phone
-        employee_info += "%s," % age
-        employee_info += "\n"
-        f.write(employee_info)
+    print("Adding a new employees, from path:\n"+str(path))
+    data = pd.read_csv(str(path))
+    data.to_csv('database.csv')
 
 
 
